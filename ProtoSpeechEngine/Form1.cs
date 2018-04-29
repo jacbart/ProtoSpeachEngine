a940ab4ff2dc084e6f3e07b748c3d812461c22db
Jack Bartlett <jacbart@gmail.com>
2018-04-29 12:33:30 -0700

    Uncommented the switch statment and add modes to grxml, File not being loaded in at the moment

--- ProtoSpeechEngine/Form1.cs
+++ ProtoSpeechEngine/Form1.cs
@@ -18,10 +18,9 @@ namespace protoSpeechEngine
     public partial class Form1 : Form
     {
         SpeechRecognizer recEngine = new SpeechRecognizer();
-        private const string grammarPath = @"C:\Users\user\Documents\GitHub\ProtoSpeechEngine\ProtoSpeechEngine\bin\Debug\";
         bool enableBtn = true;
         Grammar movementGrammar;
-        Grammar modeGrammar;
+        //Grammar modeGrammar;
         NVDA NVDAApi = new NVDA();
         String[] modes = new String[] { "Edit", "Review"};
 
@@ -45,7 +44,7 @@ namespace protoSpeechEngine
             if (enableBtn == true)
             {
                 recEngine.LoadGrammarAsync(movementGrammar);
-                recEngine.LoadGrammarAsync(modeGrammar);
+                //recEngine.LoadGrammarAsync(modeGrammar);
                 btn1.AccessibleName = "Voice Control Enabled";
                 btn1.Text = "Voice Control Enabled";
                 richTextBox1.Text += "Enabling Command Logging\n";
@@ -54,7 +53,7 @@ namespace protoSpeechEngine
             else if (enableBtn == false)
             {
                 recEngine.UnloadGrammar(movementGrammar);
-                recEngine.UnloadGrammar(modeGrammar);
+                //recEngine.UnloadGrammar(modeGrammar);
                 btn1.AccessibleName = "Voice Control Disabled";
                 btn1.Text = "Voice Control Disabled";
                 richTextBox1.Text += "Disabling Command Logging\n";
@@ -67,18 +66,21 @@ namespace protoSpeechEngine
             //Choices movementCommands = new Choices();
             //movementCommands.Add(new string[] { "Next", "Previous", "Prev", "Root" });
 
+            //string fi = Directory.GetCurrentDirectory() + @"\vocab.grxml";
+
             GrammarBuilder movementGrammarBuilder = new GrammarBuilder();
             //movementGrammarBuilder.Append(movementCommands);
-            movementGrammarBuilder.AppendRuleReference(grammarPath + "vocab.grxml");
+            movementGrammarBuilder.AppendRuleReference(Directory.GetCurrentDirectory() + @"\vocab.grxml");
             movementGrammar = new Grammar(movementGrammarBuilder);
 
-            Choices modeCommands= new Choices();
-            modeCommands.Add(modes);
 
-            GrammarBuilder modeBuilder = new GrammarBuilder();
-            modeBuilder.Append(modeCommands);
-            modeBuilder.Append("Mode");
-            modeGrammar = new Grammar(modeBuilder);
+            //Choices modeCommands= new Choices();
+            //modeCommands.Add(modes);
+
+            //GrammarBuilder modeBuilder = new GrammarBuilder();
+            //modeBuilder.Append(modeCommands);
+            //modeBuilder.Append("Mode");
+            //modeGrammar = new Grammar(modeBuilder);
 
             //recEngine.SpeechRecognized += RecEngine_SpeechRecognized;
             recEngine.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(RecEngine_SpeechRecognized);
@@ -86,24 +88,20 @@ namespace protoSpeechEngine
 
         private void RecEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
         {
-            //switch (e.Result.Text)
-            //{
-            //    case "Previous":
-            //    case "Prev":
-            //        this.SendCommand("review_previousWord");
-            //        break;
-            //    case "Next":
-            //        this.SendCommand("review_nextWord");
-            //        break;
-            //    case "Root":
-            //        this.SendCommand("navigatorObject_moveFocus|2");
-            //        break;
-            //}
+            switch (e.Result.Text)
+            {
+                case "Previous":
+                    this.SendCommand("review_previousWord");
+                    break;
+                case "Next":
+                    this.SendCommand("review_nextWord");
+                    break;
+                case "Root":
+                    this.SendCommand("navigatorObject_moveFocus|2");
+                    break;
+            }
             //String v = e.Result.Semantics.Value.ToString();
-            //richTextBox1.Text += v + "\n";
-            //richTextBox1.Text += e.Result.Text + "\n";
-            //this.SendCommand(e.Result.Semantics.Value.ToString());
-            richTextBox1.Text += e.Result.Semantics.Value;
+            richTextBox1.Text += e.Result.Text + "\n";
         }
 
         private void clear_Click(object sender, EventArgs e)
